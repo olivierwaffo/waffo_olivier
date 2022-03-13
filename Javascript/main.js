81b109bfc4932e2d4e6db781b839b5d65febbc29
Olivier WAFFO <olivier.waffo@yahoo.com>
2022-03-13 13:51:26 +0100

    create main.js file and adding first program

---
+++ Javascript/main.js
@@ -0,0 +1,17 @@
+let age = 43;
+let fullname = "Olivier WAFFO";
+let status = false;
+let schoolPlace = "Ecole Protestante Marie Coker";
+console.log(typeof age);
+console.log(typeof fullname);
+console.log(typeof status);
+console.log(typeof schoolPlace);
+
+console.log(age);
+console.log(fullname);
+console.log(status);
+console.log(schoolPlace);
+age = age + 1;
+status = true;
+console.log(age);
+console.log(status);
