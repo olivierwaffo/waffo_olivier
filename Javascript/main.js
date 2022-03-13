9f23daf730619fb269d3d6c5a61e0485f67d9534
Olivier WAFFO <olivier.waffo@yahoo.com>
2022-03-13 14:16:43 +0100

    add function (ifcondition) in main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,17 +1,32 @@
-let age = 43;
-let fullname = "Olivier WAFFO";
-let status = false;
-let schoolPlace = "Ecole Protestante Marie Coker";
-console.log(typeof age);
-console.log(typeof fullname);
-console.log(typeof status);
-console.log(typeof schoolPlace);
+function declarationExo() {
+  let age = 43;
+  let fullname = "Olivier WAFFO";
+  let status = false;
+  let schoolPlace = "Ecole Protestante Marie Coker";
+  console.log(typeof age);
+  console.log(typeof fullname);
+  console.log(typeof status);
+  console.log(typeof schoolPlace);
+  console.log(age);
+  console.log(fullname);
+  console.log(status);
+  console.log(schoolPlace);
+  age = age + 1;
+  status = true;
+  console.log(age);
+  console.log(status);
+}
 
-console.log(age);
-console.log(fullname);
-console.log(status);
-console.log(schoolPlace);
-age = age + 1;
-status = true;
-console.log(age);
-console.log(status);
+//declarationExo();
+
+function ifcondition() {
+  let temperature = 25;
+  if (temperature >= 10 && temperature <= 20) {
+    console.log("Mettre un Pull");
+  } else if (temperature < 10) {
+    console.log("Mettre un Manteau");
+  } else {
+    console.log("Mettre seulemnt un Tee-shirt");
+  }
+}
+ifcondition();
