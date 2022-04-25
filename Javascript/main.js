c9a150671993bd226ab402edaa757f25ab872c49
Olivier WAFFO <olivier.waffo@yahoo.com>
2022-04-25 13:33:20 +0200

    Mise à jour des onglets du quiz en modifiant les fichiers style index et main

--- Javascript/main.js
+++ Javascript/main.js
@@ -14,13 +14,29 @@ document.getElementById("abidjan").onclick = function () {
 //document.querySelector(".qcm").style.visibility = "hidden";
 
 document.getElementById("qcm").onclick = function () {
-  document.querySelector(".div-qcm").style.visibility = "visible";
-  document.querySelector(".div-form").style.visibility = "hidden";
+  document.getElementById("qcm").classList.remove("w3-yellow");
+  document.getElementById("qcm").classList.remove("w3-text-black");
+  document.getElementById("qcm").classList.add("w3-black");
+  document.getElementById("qcm").classList.add("w3-text-white");
+  document.getElementById("form").classList.add("w3-yellow");
+  document.getElementById("form").classList.add("w3-text-black");
+  document.getElementById("form").classList.remove("w3-black");
+  document.getElementById("form").classList.remove("w3-text-white");
+  document.querySelector(".div-qcm").hidden = false;
+  document.querySelector(".div-form").hidden = true;
 };
 
 document.getElementById("form").onclick = function () {
-  document.querySelector(".div-form").style.visibility = "visible";
-  document.querySelector(".div-qcm").style.visibility = "hidden";
+  document.getElementById("form").classList.add("w3-black");
+  document.getElementById("form").classList.add("w3-text-white");
+  document.getElementById("form").classList.remove("w3-yellow");
+  document.getElementById("form").classList.remove("w3-text-black");
+  document.getElementById("qcm").classList.remove("w3-black");
+  document.getElementById("qcm").classList.remove("w3-text-white");
+  document.getElementById("qcm").classList.add("w3-yellow");
+  document.getElementById("qcm").classList.add("w3-text-black");
+  document.querySelector(".div-form").hidden = false;
+  document.querySelector(".div-qcm").hidden = true;
 };
 
 /*Affichage de la réponse en fonction du choix de radio button*/
@@ -53,57 +69,23 @@ function validate() {
   for (var i = 0; i < checkboxes.length; i++) {
     if (checkboxes[i].checked && checkboxes[i].value === "true") {
       checkboxChecked.push(checkboxes[i]);
-    }else if (checkboxes[i].checked && checkboxes[i].value === "false") {
+    } else if (checkboxes[i].checked && checkboxes[i].value === "false") {
       checkboxChecked.pop();
     }
   }
 
-/* cette condition affiche la réponse en fonction du  quota de checkbox atteint ou pas */
-if(checkboxChecked.length === 2){
-  document.querySelector(".div-qcm").classList.add("w3-round-xlarge");
-  document.getElementById("respContainerQ2").style.backgroundColor = "green";
-  document.getElementById("responseQ2").innerHTML = "VRAI";
-  document.getElementById("responseQ2").style.color = "white";
-  document.getElementById("responseQ2").style.fontSize = 20;
-} else{
-  document.querySelector(".div-qcm").classList.remove("w3-round-xlarge");
-  document.getElementById("respContainerQ2").style.backgroundColor = "red";
-  document.getElementById("responseQ2").innerHTML = "FAUX";
-  document.getElementById("responseQ2").style.color = "white";
-  document.getElementById("responseQ2").style.fontSize = 20;
-}
-}
-
-/*affiche les differentes déclarations d'elements*/
-function declarationExo() {
-  let age = 43;
-  let fullname = "Olivier WAFFO";
-  let status = false;
-  let schoolPlace = "Ecole Protestante Marie Coker";
-  console.log(typeof age);
-  console.log(typeof fullname);
-  console.log(typeof status);
-  console.log(typeof schoolPlace);
-  console.log(age);
-  console.log(fullname);
-  console.log(status);
-  console.log(schoolPlace);
-  age = age + 1;
-  status = true;
-  console.log(age);
-  console.log(status);
-}
-
-/*Implémentaion de la condition if statement*/
-function ifcondition() {
-  let temperature = 25;
-  if (temperature >= 10 && temperature <= 20) {
-    console.log("Mettre un Pull");
-  } else if (temperature < 10) {
-    console.log("Mettre un Manteau");
+  /* cette condition affiche la réponse en fonction du  quota de checkbox atteint ou pas */
+  if (checkboxChecked.length === 2) {
+    document.querySelector(".div-qcm").classList.add("w3-round-xlarge");
+    document.getElementById("respContainerQ2").style.backgroundColor = "green";
+    document.getElementById("responseQ2").innerHTML = "VRAI";
+    document.getElementById("responseQ2").style.color = "white";
+    document.getElementById("responseQ2").style.fontSize = 20;
   } else {
-    console.log("Mettre seulemnt un Tee-shirt");
+    document.querySelector(".div-qcm").classList.remove("w3-round-xlarge");
+    document.getElementById("respContainerQ2").style.backgroundColor = "red";
+    document.getElementById("responseQ2").innerHTML = "FAUX";
+    document.getElementById("responseQ2").style.color = "white";
+    document.getElementById("responseQ2").style.fontSize = 20;
   }
 }
-//declarationExo();
-//ifcondition();
